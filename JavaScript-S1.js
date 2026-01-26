01f88270afbdf977b8ee46cc02b1b975d83a6859
Ahmad <ahd.ajjawe@gmail.com>
2026-01-26 18:29:39 +0100

    update1

---
+++ JavaScript-S1.js
@@ -0,0 +1,40 @@
+
+// x=5
+// y='Hello'
+// z=true 
+// a=[1,2,3,4]
+// m=null
+// n=undefined
+
+// console.log(typeof x)
+// console.log(typeof y)
+// console.log(typeof z)
+// console.log(typeof a)
+// console.log(typeof m)
+// console.log(typeof n)
+
+
+// x='Hello'
+// console.log(x[2])
+
+
+//    0,1,2,3,4
+// x=[1,2,3,4,5]
+// x[2]='hi'
+// console.log(x)
+
+// str='Hello'
+// str[1]='m'
+// console.log(str)
+
+// console.log(str.length)
+
+// console.log(x[x.length-1]) 
+
+
+let x=5
+var y=2
+const  z=6
+
+x=11
+y=99
